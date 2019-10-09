15797ccc99c710449110f85f6cf07d4c42c1de9e
Juho Härme <juho.harme@utu.fi>
2019-10-09 14:12:12 +0300

    refactor publication to stateless

--- src/components/content/publication/index.jsx
+++ src/components/content/publication/index.jsx
@@ -5,39 +5,40 @@ import FoldableBox from '../../ui/foldablebox/index.jsx';
 import RaisableBox from '../../ui/raisableBox/index.jsx';
 import styles from './publications.scss';
 
-export default class Publication extends Component {
-  render() {
-    const { details, dispatch, publications, pretitle } = this.props;
-    const { title, receptions, receptionOf, _id, ...fields } = details;
-    return (
-      <RaisableBox header={`${pretitle ? `${pretitle} : ` : ''}${title}`}>
-        <div className={styles.idline}>{_id}</div>
-        <ul className={styles.details}>
-          {Object.keys(fields)
-            .filter(field => fields[field])
-            .map(field => (
-              <li>
-                <span>{field}:</span>
-                <span>{fields[field]}</span>
-              </li>
-            ))}
-        </ul>
-        <FoldableBox header="Reseptiot" onOpen={() => dispatch(fetchReceptions(_id))}>
-          {Object.keys(receptions).map(key =>
-            receptions[key].map(
-              rId =>
-                publications[rId] && (
-                  <Publication
-                    pretitle={key.replace(/s$/, '')}
-                    details={publications[rId]}
-                    publications={publications}
-                    dispatch={dispatch}
-                  />
-                )
-            )
-          )}
-        </FoldableBox>
-      </RaisableBox>
-    );
-  }
-}
+const Publication = props => {
+  const { details, dispatch, publications, pretitle } = props;
+  const { title, receptions, receptionOf, _id, ...fields } = details;
+  return (
+    <RaisableBox header={`${pretitle ? `${pretitle} : ` : ''}${title}`}>
+      <div className={styles.idline}>{_id}</div>
+      <ul className={styles.details}>
+        {Object.entries(fields)
+          .filter(([field, val]) => val && !['target', 'reception_type', 'tempId'].includes(field))
+          .map(([field, val]) => (
+            <li>
+              <span>{field}:</span>
+              <span>{val}</span>
+            </li>
+          ))}
+      </ul>
+      <FoldableBox header="Reseptiot" onOpen={() => dispatch(fetchReceptions(_id))}>
+        {Object.keys(receptions).map(key =>
+          receptions[key].map(
+            rId =>
+              publications[rId] && (
+                <Publication
+                  pretitle={key.replace(/s$/, '')}
+                  details={publications[rId]}
+                  publications={publications}
+                  dispatch={dispatch}
+                />
+              )
+          )
+        )}
+      </FoldableBox>
+    </RaisableBox>
+  );
+};
+
+export default Publication;
+
